31f6ca96a4dd565f35e7a7f9e87fad24e461dbc4
casparzzz123 <80700684+casparzzz123@users.noreply.github.com>
2022-03-15 21:06:34 +0100

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,50 @@
+import os
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+
+root = Tk()
+
+
+try:
+        Tk().withdraw()
+        filename = askopenfilename()
+        print(filename)
+        with open(filename) as file:
+            found = 0
+            damage = 0  
+            for line in file:
+                words = line.split()
+                for word in words:
+                ## checks for virus
+                    if word == "VIRUS":
+                        found = found + 1
+                        print("found a virus") 
+                ## checks for notepad virus
+                    if word == "notepad":
+                        found = found + 1
+                        print("found a virus")
+                ## checks for Deletion Virus
+                    if word == "DEL":
+                        found = found + 1
+                        print("found a virus")
+                ## checks for shutdown virus
+                    if word == "shutdown":
+                        found = found + 1
+                        print("found a virus")
+                ## checks for msg box virus
+                    if word == "reg":
+                        found = found + 1
+                        damage = damage + 1
+                        print("found a SERVERLY VIRUS")
+                
+
+        print("You Have " + str(found) + " Viruses!")
+        print("You Have " + str(damage) + " SEVERLY VIRUSES!")      
+
+        print("PRESS ENTER TO EXIT")
+        input("e")
+        print("Bye!")
+except FileNotFoundError:
+        print("Error: No Files Found?")
+
+    
